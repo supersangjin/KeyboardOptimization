c66239f9de5a8e7759ca71edba899f987f64472e
최상진 <hhclover@kaist.ac.kr>
2017-11-22 13:48:03 +0900

    main.py

--- main.py
+++ main.py
@@ -1,34 +1,26 @@
 import getopt # get input data
 
-class Data:
-    def __init__(self, alphabet, x): # target alphabet, user input x-coordinate
-        self.alphabet = alphabet
-        self.x = x
 
-    def getUser(self):
-        return self.user
-
-    def getAlphabet(self):
-        return self.alphabet
-
-    def getX(self):
-        return self.x
-
-class DataList: # Collection of user data
-    def __init__(self, user):
-        self.dataList = []
-        self.user = user
-
-    def addData(self, data):
-        self.dataList.append(data)
+WIDTH_KEY_DEFAULT = 100
+WIDTH_EMPTY_DEFAULT = 5
+START_FIRST_AXIS = 0
+START_SECOND_AXIS = 20
+START_THIRD_AXIS = START_SECOND_AXIS + WIDTH_KEY_DEFAULT + WIDTH_EMPTY_DEFAULT
+WIDTH_KEY_AND_EMPTY = WIDTH_KEY_DEFAULT + WIDTH_EMPTY_DEFAULT
+DEFAULT_END_AXIS = 9 * WIDTH_KEY_AND_EMPTY + WIDTH_KEY_DEFAULT
+NUM_ITERATION = 1
 
 class Node:
     def __init__(self, alphabet, x): # alphabet, center x-coordinate,
         self.alphabet = alphabet
         self.x = x
+        self.original_weight = 0 # 0 ~ 10
         self.weight = 0 # 0 ~ 10
-        self.width = 0
-
+        self.max_weight = 0 # 0 ~ 10
+        self.width = WIDTH_KEY_DEFAULT
+        self.normalized_left = WIDTH_EMPTY_DEFAULT/2
+        self.normalized_right = WIDTH_EMPTY_DEFAULT/2
+        
     def getAlphabet(self):
         return self.alphabet
 
@@ -40,6 +32,9 @@ class Node:
 
     def getWidth(self):
         return self.width
+    
+    def get_N_left_right(self):
+        return (self.normalized_left, self.normalized_right)
 
 class Layer:
     def __init__(self, layer): # layer = 0 or 1 or 2
@@ -48,45 +43,91 @@ class Layer:
 
     def initialize(self):
         if (self.layer == 0):
-            q = Node('q',0.0)
-            w = Node('w',0.0)
-            e = Node('e',0.0)
-            r = Node('r',0.0)
-            t = Node('t',0.0)
-            y = Node('y',0.0)
-            u = Node('u',0.0)
-            i = Node('i',0.0)
-            o = Node('o',0.0)
-            p = Node('p',0.0)
+            q = Node('q',START_FIRST_AXIS)
+            w = Node('w',START_FIRST_AXIS + WIDTH_KEY_AND_EMPTY)
+            e = Node('e',START_FIRST_AXIS + 2 * WIDTH_KEY_AND_EMPTY)
+            r = Node('r',START_FIRST_AXIS + 3 * WIDTH_KEY_AND_EMPTY)
+            t = Node('t',START_FIRST_AXIS + 4 * WIDTH_KEY_AND_EMPTY)
+            y = Node('y',START_FIRST_AXIS + 5 * WIDTH_KEY_AND_EMPTY)
+            u = Node('u',START_FIRST_AXIS + 6 * WIDTH_KEY_AND_EMPTY)
+            i = Node('i',START_FIRST_AXIS + 7 * WIDTH_KEY_AND_EMPTY)
+            o = Node('o',START_FIRST_AXIS + 8 * WIDTH_KEY_AND_EMPTY)
+            p = Node('p',START_FIRST_AXIS + 9 * WIDTH_KEY_AND_EMPTY)
             self.nodeList = [q, w, e, r, t, y, u, i, o, p]
         elif (self.layer == 1):
-            a = Node('a',0.0)
-            s = Node('s',0.0)
-            d = Node('d',0.0)
-            f = Node('f',0.0)
-            g = Node('g',0.0)
-            h = Node('h',0.0)
-            j = Node('j',0.0)
-            k = Node('k',0.0)
-            l = Node('l',0.0)
+            a = Node('a',START_SECOND_AXIS)
+            s = Node('s',START_SECOND_AXIS + WIDTH_KEY_AND_EMPTY)
+            d = Node('d',START_SECOND_AXIS + 2 * WIDTH_KEY_AND_EMPTY)
+            f = Node('f',START_SECOND_AXIS + 3 * WIDTH_KEY_AND_EMPTY)
+            g = Node('g',START_SECOND_AXIS + 4 * WIDTH_KEY_AND_EMPTY)
+            h = Node('h',START_SECOND_AXIS + 5 * WIDTH_KEY_AND_EMPTY)
+            j = Node('j',START_SECOND_AXIS + 6 * WIDTH_KEY_AND_EMPTY)
+            k = Node('k',START_SECOND_AXIS + 7 * WIDTH_KEY_AND_EMPTY)
+            l = Node('l',START_SECOND_AXIS + 8 * WIDTH_KEY_AND_EMPTY)
             self.nodeList = [a, s, d, f, g, h, j, k, l]
         elif (self.layer == 2):
-            z = Node('z',0.0)
-            x = Node('x',0.0)
-            c = Node('c',0.0)
-            v = Node('v',0.0)
-            b = Node('b',0.0)
-            n = Node('n',0.0)
-            m = Node('m',0.0)
+            z = Node('z',START_THIRD_AXIS)
+            x = Node('x',START_THIRD_AXIS + WIDTH_KEY_AND_EMPTY)
+            c = Node('c',START_THIRD_AXIS + 2 * WIDTH_KEY_AND_EMPTY)
+            v = Node('v',START_THIRD_AXIS + 3 * WIDTH_KEY_AND_EMPTY)
+            b = Node('b',START_THIRD_AXIS + 4 * WIDTH_KEY_AND_EMPTY)
+            n = Node('n',START_THIRD_AXIS + 5 * WIDTH_KEY_AND_EMPTY)
+            m = Node('m',START_THIRD_AXIS + 6 * WIDTH_KEY_AND_EMPTY)
             self.nodeList = [z, x, c, v, b, n, m]
 
     def normalize(self): # TODO calculate x-coordnate and width according to each node's weight
+        for i in range(len(self.nodeList)):
+            if(i != (len(self.nodeList) - 1)):
+                diff = self.nodeList[i].weight - self.nodeList[i+1].weight
+                self.nodeList[i].normalized_right = WIDTH_EMPTY_DEFAULT / 2 + (diff / 10.0) * (WIDTH_EMPTY_DEFAULT / 2)
+            if(i != 0):
+                diff = self.nodeList[i].weight - self.nodeList[i-1].weight
+                self.nodeList[i].normalized_left = WIDTH_EMPTY_DEFAULT / 2 + (diff / 10.0) * (WIDTH_EMPTY_DEFAULT / 2)
+        
+        
+    def calculateFitness(self, data_list): # TODO calculate fitness of the layer
+        num = 0
+        for i in range(len(data_list)):
+            node = self.get_node(data_list[i][0])
+            l_and_r = node.get_N_left_right()
+            if((max(0, node.getX() - l_and_r[0]) <= (int)(data_list[i][1])) and ((int)(data_list[i][1]) <= min(node.getX() + WIDTH_KEY_DEFAULT + l_and_r[1], DEFAULT_END_AXIS))):
+                num = num + 1
+        return num
+    
+    def get_node(self, char): # get node from the alphabet. 'a' -> get node a
+        for i in range(len(self.nodeList)):
+            if(char == self.nodeList[i].getAlphabet()):
+                return self.nodeList[i]        
 
-    def calculateFitness(self): # TODO calculate fitness of the layer
-
-    def
-
-
+    def explore_neighborhood(self, data_list): # search all the neighborhoods of given layer
+        max_fitness = self.calculateFitness(data_list)
+        for i in range(len(self.nodeList)): # original weight is assumed to be maximum, at first.
+            self.nodeList[i].max_weight = self.nodeList[i].original_weight
+        permutation_list = []
+        for i in range(len(self.nodeList)): # to search all the neighborhoods, make permutation
+            permutation_list.append([-1, 0, 1])
+        permutation_list = make_permutation(permutation_list)
+        for i in range(len(permutation_list)):
+            end = 0
+            for j in range(len(self.nodeList)):
+                self.nodeList[j].weight = self.nodeList[j].original_weight + permutation_list[i][j]
+                if(self.nodeList[j].weight < 0 or self.nodeList[j].weight > 10):
+                    end = 1
+                    break
+            if(end == 1):
+                continue
+            self.normalize() # apply the weight to change the space in between nodes
+            new_fitness = self.calculateFitness(data_list)
+            if(new_fitness >= max_fitness):
+                for k in range(len(self.nodeList)):
+                    self.nodeList[k].max_weight = self.nodeList[k].weight
+                    max_fitness = new_fitness
+        for i in range(len(self.nodeList)):
+            self.nodeList[i].original_weight = self.nodeList[i].max_weight
+            self.nodeList[i].weight = self.nodeList[i].max_weight
+        self.normalize()
+        
+        
 class Keyboard:
     def __init__(self, user):
         self.layerList = []
@@ -111,15 +152,68 @@ class Keyboard:
 
     def getLayer(self, n): # return nth layer
         return self.layerList[n]
+            
+    def calculateFitness(self, data_list): # to optimize each layer, return fitness_list : list of fitness of each layer
+        Fitness_List = []
+        for i in range(3):
+            Fitness_List.append (self.getLayer(i).calculateFitness(data_list[i]))
+        return Fitness_List
 
-    def calculateFitness(self):
+    def get_layer(self, char): # from the alphabet, get the layer. ex) q -> layer 0, a -> layer 1, z -> layer 2
+        for i in range(3):
+            for elem in self.getLayer(i).nodeList:
+                if(char == elem.getAlphabet()):
+                    return i
+
+            
+    def seperate_data(self, data_list): # make new list of 3 lists from the data list, each list is containing data for each layer
+        new_data_list = [[], [], []]
+        for i in range(len(data_list)):
+            new_data_list[self.get_layer(data_list[i][0])].append(data_list[i])
+        return new_data_list
+
+    def explore_neighborhood(self, data_list): # search all the neighborhoods
+        for i in range(3):
+            self.getLayer(i).explore_neighborhood(data_list[i])
+            
+
+def parse_data(file_name):
+    file = open(file_name, 'r')
+    data_list = file.readlines()
+    for i in range(len(data_list)):
+        data_list[i] = data_list[i].strip('\n')
+        data_list[i] = data_list[i].split(' ')
+    return data_list
 
 
-if __name__ == 'main':
+def make_permutation(permutation_list): # to explore all the neighborhoods, we need permutation. like n = 2 -> [-1,-1], [-1,0], [-1,1], ... , [1,1], the numbers to apply for each weight
+    if(len(permutation_list) == 1):
+        new_list = []
+        for i in range(len(permutation_list[0])):
+            new_list.append([permutation_list[0][i]])
+        return new_list
+    prev_comb_list = make_permutation(permutation_list[1:len(permutation_list)])
+    new_list = []
+    for i in range(len(permutation_list[0])):
+        for j in range(len(prev_comb_list)):
+            append_list = [permutation_list[0][i]]
+            append_list.extend(prev_comb_list[j])
+            new_list.append(append_list)
+    return new_list
+    
+    
+if __name__ == '__main__':
     # parse data from input
-
+    file_name = "input.txt"
+    data_list = parse_data(file_name)
+    
     # make Keyboard
     keyboard = Keyboard('user')
     keyboard.initialize()
 
+    # seperate data to each layer
+    data_list = keyboard.seperate_data(data_list)
+    
     # TODO make search problem
+    for i in range(NUM_ITERATION):
+        keyboard.explore_neighborhood(data_list)
